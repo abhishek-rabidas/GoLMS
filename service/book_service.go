2374a8d7c38ad8c7bc17e020db099ca9b77422ee
abhishek-rabidas <avisec07@gmail.com>
2024-03-18 23:04:40 +0530

    book service: getAllBooks

--- service/book_service.go
+++ service/book_service.go
@@ -1,6 +1,12 @@
 package service
 
-import "gomvc/views"
+import (
+	"encoding/csv"
+	log "github.com/sirupsen/logrus"
+	"gomvc/model"
+	"gomvc/views"
+	"os"
+)
 
 type BookService struct {
 }
@@ -9,6 +15,73 @@ func NewBookService() *BookService {
 	return &BookService{}
 }
 
+// GetBooks TODO: pagination
 func (b *BookService) GetBooks(userType string) *[]views.BookResponse {
-	return nil
+	var books []views.BookResponse
+
+	file, err := os.Open("resources/regularUser.csv")
+
+	if err != nil {
+		log.Error(err)
+		return nil
+	}
+
+	defer file.Close()
+
+	csvReader := csv.NewReader(file)
+	records, err := csvReader.ReadAll()
+
+	if err != nil {
+		log.Error(err)
+		return nil
+	}
+
+	for i, record := range records {
+		if i == 0 {
+			continue
+		}
+
+		book := views.BookResponse{
+			BookName:        record[0],
+			Author:          record[1],
+			PublicationYear: record[2],
+		}
+
+		books = append(books, book)
+	}
+
+	if userType == model.Admin {
+		file, err = os.Open("resources/adminUser.csv")
+
+		if err != nil {
+			log.Error(err)
+			return nil
+		}
+
+		defer file.Close()
+
+		csvReader = csv.NewReader(file)
+		records, err = csvReader.ReadAll()
+
+		if err != nil {
+			log.Error(err)
+			return nil
+		}
+
+		for i, record := range records {
+			if i == 0 {
+				continue
+			}
+
+			book := views.BookResponse{
+				BookName:        record[0],
+				Author:          record[1],
+				PublicationYear: record[2],
+			}
+
+			books = append(books, book)
+		}
+	}
+
+	return &books
 }
--- views/BookResponse.go
+++ views/BookResponse.go
@@ -3,5 +3,5 @@ package views
 type BookResponse struct {
 	BookName        string `json:"book_name"`
 	Author          string `json:"author"`
-	PublicationYear int    `json:"publication_year"`
+	PublicationYear string `json:"publication_year"`
 }
